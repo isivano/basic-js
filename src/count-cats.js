86b8625fd10e678199458f68b468b265efb9ca69
Ivan Ivanov <ivanov-i.s@mail.ru>
2021-03-09 21:33:00 +0300

    fix: correct count-cats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(backyard) {
   let res = 0;
   backyard.forEach(element => element.forEach(item => {
     if (item === "^^") res++;
