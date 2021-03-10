82da0a9a899d6804a7cdc07b26f3fb77f78d0445
Ivan Ivanov <ivanov-i.s@mail.ru>
2021-03-10 11:50:35 +0300

    feat: implement dream-team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(team) {
+  if (Array.isArray(team)){
+    let dreamteam = [];
+    team.forEach(element => {
+      if (typeof(element) === "string") {
+        element = element.trim();
+        dreamteam.push(element[0].toUpperCase());
+      }
+    });
+    if (team.length !== 0){
+      dreamteam.sort();
+      return dreamteam.join("");
+    } else  return false;
+} else  return false;
 };
+
+
