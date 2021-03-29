717099690992c57d1a0616cdbfa82d37ee90c8cb
Ivan Ivanov <ivanov-i.s@mail.ru>
2021-03-29 15:49:21 +0300

    feat: implement chainMaker function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    value === undefined ? this.chain.push(" ") : this.chain.push("" + value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!Number.isInteger(position) || position - 1 < 0 || position - 1 > this.chain.length) {
+      this.chain = [];
+      throw new Error()
+    };
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let res = `( ${this.chain.join(" )~~( ")} )`;
+    this.chain = [];
+    return res;
   }
 };
 
