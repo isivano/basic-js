2694d77ff1662146a17886cc9e68110534baad1d
Ivan Ivanov <ivanov-i.s@mail.ru>
2021-03-29 19:47:19 +0300

    feat: implement repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const letsRepeat = (string, times, separator) => {
+    if (!Number.isInteger(times)) return string;
+    let arr = [];
+    string = "" + string;
+    while (times > 0) { arr.push(string); times--; }
+    return arr.join(separator);
+  }
+  let res = "";
+  if (options.separator === undefined) options.separator = "+";
+  if (options.additionSeparator === undefined) options.additionSeparator = "|";
+  if (options.addition !== undefined)
+    res += letsRepeat(options.addition, options.additionRepeatTimes, options.additionSeparator);
+  return letsRepeat(str + res, options.repeatTimes, options.separator);
 };
-  
