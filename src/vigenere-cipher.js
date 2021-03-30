66e2b7247e2092a8675eb8520344f8baede506b5
Ivan Ivanov <ivanov-i.s@mail.ru>
2021-03-30 19:38:46 +0300

    feat: implement VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,60 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  constructor(direct) {
+    this.directMachine = (direct === undefined || direct === true) ? true : false;
+  }
+
+  getEncryptMatrix(key) {
+    let encryptMatrix = [];
+    for (let i = 0; i < key.length; i++) {
+      let shiftedString = "";
+      shiftedString = this.alphabet.substr(this.alphabet.indexOf(key[i]), 26);
+      encryptMatrix.push(shiftedString.split("")); // 
+    }
+    return encryptMatrix;
+  }
+
+  encrypt(string, key) {
+    key = key.toUpperCase();
+    string = string.toUpperCase();
+    let encryptMatrix = this.getEncryptMatrix(key);
+    let resultArray = [];
+    let j = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (this.alphabet.includes(string[i])) {
+        if (j === key.length) j = 0;
+        let posX, posY;
+        posX = j;
+        posY = this.alphabet.indexOf(string[i]);
+        resultArray.push(encryptMatrix[posX][posY]);
+        j++;
+      } else {
+        resultArray.push(string[i]);
+      }
+    }
+    return this.directMachine ? resultArray.join("") : resultArray.reverse().join("");
+  }
+
+  decrypt(string, key) {
+    key = key.toUpperCase();
+    string = string.toUpperCase();
+    let encryptMatrix = this.getEncryptMatrix(key);
+    let resultArray = [];
+    let j = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (this.alphabet.includes(string[i])) {
+        if (j === key.length) j = 0;
+        let charIndex = encryptMatrix[j].indexOf(string[i]);
+        resultArray.push(this.alphabet.charAt(charIndex));
+        j++;
+      } else {
+        resultArray.push(string[i]);
+      }
+    }
+    return this.directMachine ? resultArray.join("") : resultArray.reverse().join("");
   }
 }
 
