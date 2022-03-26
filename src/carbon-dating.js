3b10a0a4ecb6f517e30d1a44c50d386e4da6efb2
Ivan Ivanov <ivanov-i.s@mail.ru>
2022-03-26 14:55:33 +0300

    feat: implement dateSample function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,8 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
-<<<<<<< HEAD
-module.exports = function dateSample(sampleActivity) {
+function dateSample(sampleActivity) {
   if (typeof(sampleActivity)==="string" && isFinite(+sampleActivity)){
     sampleActivity = +sampleActivity;
     if (sampleActivity>0 && sampleActivity<15){
@@ -14,27 +13,8 @@ module.exports = function dateSample(sampleActivity) {
     }
   }
   return false;
-=======
-/**
- * Determine the age of archeological find by using
- * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
- * @return {Number | Boolean} calculated age in years or false
- * in case of incorrect sampleActivity
- *
- * @example
- * 
- * dateSample('1') => 22387
- * dateSample('WOOT!') => false
- *
- */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
 }
 
 module.exports = {
   dateSample
->>>>>>> c7b6e4ca24545c5e98dd7883637123d56d3f4d9f
 };
